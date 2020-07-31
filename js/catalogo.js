11bfa5372ae044f57674105ef790d6c098e336e7
juanmurillog <jjmurillob@ut.edu.co>
2020-07-30 22:21:35 -0500

    Agregado el boton comprar con enlaces a la api de wsp y mensaje personalizado

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,4 +1,7 @@
 
+const linkWspCaja = 'https://api.whatsapp.com/send?phone=573155192405&text=Hola%20quiero%20informacion%20acerca%20de%20la%20caja%20china%20';
+const linkWspRamen = 'https://api.whatsapp.com/send?phone=573155192405&text=Hola%20quiero%20informacion%20acerca%20de%20el%20ramen'
+const linkWspSushi = 'https://api.whatsapp.com/send?phone=573155192405&text=Hola%20quiero%20informacion%20acerca%20de%20el%20sushi'
 document.addEventListener('DOMContentLoaded', () => {
 
     console.log('cargado')
@@ -14,3 +17,16 @@ document.addEventListener('DOMContentLoaded', () => {
 function clickHome(){
     window.location.href= './index.html'
 }
+function comprarCaja () {
+
+  window.location = linkWspCaja;
+}
+function comprarRamen () {
+
+    window.location = linkWspRamen;
+  }
+  function comprarSushi () {
+
+    window.location = linkWspSushi;
+  }
+
