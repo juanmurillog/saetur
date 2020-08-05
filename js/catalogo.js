d1d92a418b2bbb7d4d82b32c563bf1868cd84793
juanmurillog <jjmurillob@ut.edu.co>
2020-08-05 16:35:46 -0500

    agregado slider

--- js/catalogo.js
+++ js/catalogo.js
@@ -30,3 +30,14 @@ function comprarRamen () {
     window.location = linkWspSushi;
   }
 
+
+  $(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
